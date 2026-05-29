090178cd13035138a1665287906b535d18dcdae5
Makro <makro@haulis.com>
2026-05-29 13:27:00 +0200

    Bump versionCode to 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.scamslayer.android"
         minSdk = 29
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.3.0"
+        versionCode = 13
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
